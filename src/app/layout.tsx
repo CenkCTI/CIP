7c94aabe0bd1f266a3f1d9d8ee55be9a5349cf2c
CenkCTI <cenk.mcan.polat@proton.me>
2026-07-27 02:30:39 +0200

    Load CITEM background override

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 import "@xyflow/react/dist/style.css";
+import "./background-overrides.css";
 
 export const metadata: Metadata = {
   title: {
